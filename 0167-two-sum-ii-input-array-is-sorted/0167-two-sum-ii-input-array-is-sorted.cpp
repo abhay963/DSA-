74f697614f673d8b176bf33b18a100d499ec6c07
Abhay Kumar Yadav <147247437+abhay963@users.noreply.github.com>
2026-01-28 23:17:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (99.83%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+int bs(int target,int low,int high,vector<int>& nums){
+  while(low<=high){
+    int mid=low+(high-low)/2;
+    if(nums[mid]==target)return mid;
+    if(nums[mid]>target){
+high=mid-1;
+    }
+    else{
+low=mid+1;
+    }
+  }
+  return -1;
+
+}
     vector<int> twoSum(vector<int>& numbers, int target) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<numbers.size();i++){
-          mp[numbers[i]]=i;
-        }
-
-
         for(int i=0;i<numbers.size();i++){
           int num=numbers[i];
-          int need=target-numbers[i];
-          if(mp.find(need)!=mp.end()){
-            return {i+1,mp[need]+1};
-          }
+          int need=target-num;
+          int idx=bs(need,i+1,numbers.size()-1,numbers);
+          if(idx!=-1)return {i+1,idx+1};
         }
         return {};
     }
