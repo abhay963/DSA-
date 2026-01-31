d571f90c02c48b3a6b75378b0ab37dddc97b7b5c
Abhay Kumar Yadav <147247437+abhay963@users.noreply.github.com>
2026-01-31 17:41:46 +0530

    Time: 0 ms (100%), Space: 11.8 MB (79.06%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(int it:arr)mp[it]++;
+
+        unordered_set<int>st;
+        for(auto& it:mp){
+          if(st.find(it.second)!=st.end())return false;
+          st.insert(it.second);
+        }
+        return true;
+    }
+};
