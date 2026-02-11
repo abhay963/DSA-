7c9ac39f9120a57fb2e1b7728e05200e66190a14
Abhay Kumar Yadav <147247437+abhay963@users.noreply.github.com>
2026-02-11 19:46:52 +0530

    Time: 0 ms (100%), Space: 8.9 MB (66.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,12 +3,13 @@ public:
     bool isValid(string s) {
         stack<char>st;
         for(int i=0;i<s.size();i++){
-          if(s[i]=='(' || s[i]=='['|| s[i]=='{')st.push(s[i]);
-          else{
+          if(s[i]=='(' || s[i]=='[' || s[i]=='{')st.push(s[i]);
+          else {
             if(st.empty())return false;
-            char ch=st.top();
-            if((ch=='(' && s[i]==')' )|| (ch=='{' && s[i]=='}') || 
-            (ch=='[' && s[i]==']'))st.pop();
+            if((st.top()=='(' && s[i]==')') ||
+            (st.top()=='[' && s[i]==']')||
+            
+            (st.top()=='{' && s[i]=='}')   )st.pop();
             else return false;
           }
         }
