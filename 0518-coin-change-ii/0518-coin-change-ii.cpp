c458128765d298e0a46fd7dccaac3b90298b8bd4
Abhay Kumar Yadav <147247437+abhay963@users.noreply.github.com>
2026-04-03 12:34:27 +0530

    Time: 37 ms (36.21%), Space: 35.9 MB (63.71%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -16,8 +16,21 @@ vector<vector<int>>& dp){
 }
     int change(int amount, vector<int>& coins) {
       int n=coins.size();
-      vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-      return solve(n-1,amount,coins,dp);
-     
+     vector<int>prev(amount+1,0);
+     for(int i=0;i<=amount;i++){
+      if(i%coins[0]==0)prev[i]=1;
+     }
+
+     for(int i=1;i<n;i++){
+      vector<int>curr(amount+1,0);
+for(int target=0;target<=amount;target++){
+ long  nottake=prev[target];
+  long take=0;
+  if(coins[i]<=target)take=curr[target-coins[i]];
+  curr[target]=take+nottake;
+}
+prev=curr;
+     }
+     return prev[amount];
     }
 };
