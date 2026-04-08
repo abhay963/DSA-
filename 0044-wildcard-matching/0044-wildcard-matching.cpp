d0f8ca16487e0197ef9f6a7569fa28bfa5b1cc47
Abhay Kumar Yadav <147247437+abhay963@users.noreply.github.com>
2026-04-08 11:15:46 +0530

    Time: 58 ms (40.25%), Space: 18.7 MB (42.02%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -21,7 +21,8 @@ bool solve(int i,int j,string& s,string& p,vector<vector<bool>>& dp){
          int n=s.size();
         int m=p.size();
         vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
-        dp[0][0]=true;
+        vector<bool>prev(m+1,false);
+        prev[0]=true;
        
         for(int j=1;j<=m;j++){
           bool fl=true;
@@ -30,20 +31,22 @@ bool solve(int i,int j,string& s,string& p,vector<vector<bool>>& dp){
       if(p[k-1]!='*')fl= false;
       
         }
-        dp[0][j]=fl;
+        prev[j]=fl;
         }
 
        
 
         for(int i=1;i<=n;i++){
+          vector<bool>curr(m+1,false);
           for(int j=1;j<=m;j++){
 
             
-  if(s[i-1]==p[j-1]|| p[j-1]=='?') dp[i][j]= dp[i-1][j-1];
- else if(p[j-1]=='*') dp[i][j]=dp[i-1][j]||dp[i][j-1]; 
-   else dp[i][j]= false;
+  if(s[i-1]==p[j-1]|| p[j-1]=='?') curr[j]= prev[j-1];
+ else if(p[j-1]=='*') curr[j]=prev[j]||curr[j-1]; 
+   else curr[j]= false;
           }
+          prev=curr;
         }
-      return  dp[n][m];
+      return  prev[m];
     }
 };
